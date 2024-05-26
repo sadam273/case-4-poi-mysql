4d74b5ffff3ba2bd2f950eac4a1c8401730787b3
Sadam <msadamramli@gmail.com>
2024-05-26 17:27:47 +0700

    menambah fitur marker saat map diklik

---
+++ script.js
@@ -0,0 +1,22 @@
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+map.on("click", function (e) {
+  // Get the latitude and longitude from the event object
+  var lat = e.latlng.lat;
+  var lng = e.latlng.lng;
+
+  // Output the coordinates to the console
+  console.log("Coordinates: " + lat + ", " + lng);
+
+  // Optionally, you can add a marker at the clicked location
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup("Coordinates: " + lat + ", " + lng)
+    .openPopup();
+});
